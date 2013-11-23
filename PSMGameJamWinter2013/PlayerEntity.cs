5e98bed819a6c6e072498bdde00a1a4825a3c651
kazuhiro <k2.wanko@gmail.com>
2013-11-23 23:36:28 +0900

    maine scene

---
+++ PSMGameJamWinter2013/PlayerEntity.cs
@@ -0,0 +1,13 @@
+using System;
+
+namespace PSMGameJamWinter2013
+{
+	public class PlayerEntity : Entity
+	{
+		public PlayerEntity ()
+		{
+			setTextureArray(new string[]{"maou.png", "maou2.png"});
+		}
+	}
+}
+
