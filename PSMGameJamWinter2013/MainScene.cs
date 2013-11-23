dfc7112d0ab0a3181b42a091e35d674eed7afd97
yoshida <steel_x_pudding@yahoo.co.jp>
2013-11-23 17:58:11 +0900

    テスト作成

--- PSMGameJamWinter2013/MainScene.cs
+++ PSMGameJamWinter2013/MainScene.cs
@@ -10,14 +10,42 @@ using Sce.PlayStation.Core.Imaging; // for Font
 using Sce.PlayStation.HighLevel.GameEngine2D;
 using Sce.PlayStation.HighLevel.GameEngine2D.Base;
 
+using ApmFw;
+
 namespace PSMGameJamWinter2013
 {
-	public class MainScene
+	public class MainScene : GameScene
 	{
+		private SpriteForTouch message = new SpriteForTouch();
 		public MainScene ()
 		{
-			
 		}
+		public override Scene Initialize()
+		{
+			scene.Camera.SetViewFromViewport();
+			GameLog.DebugLog.Log("start");
+			
+			message.DrawSprite("DrawSpriteSample",
+										120f,
+										120f,
+										30,
+										new ImageColor(255,255,255,255),
+										scene,
+			                             true);
+			return scene;
+		}
+		
+		public override void Update(){
+		
+		if(InputDevice.CircleButton()){
+				ChangeScene( () => {return new AudioSample();} );
+			}
+			
+		}//Update()
+		public override void Terminate()
+		{
+		}
+		
 	}
 }
 
---
+++ PSMGameJamWinter2013/TestScene1.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace PSMGameJamWinter2013
+{
+	public class TestScene1
+	{
+		public TestScene1 ()
+		{
+		}
+	}
+}
+
